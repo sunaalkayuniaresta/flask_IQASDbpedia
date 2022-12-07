1d87d8e8c1b2f974fc2e4f1055f2b9f43b7ab3fb
sunaalkayuniaresta <sunaalkayuni@gmail.com>
2022-12-07 13:40:18 +0700

    minor: add main

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import re
 from App import preprocessing
 
 masukan="Siapa nama presenter yang Lahir di Jakarta? %&^[]"
-lower = preprocessing.get_input(preprocessing,masukan)
 
-pekerjaan,lahir = preprocessing.get_query(preprocessing,lower)
 
-query = preprocessing.set_sparql(preprocessing,pekerjaan,lahir)
+valid=re.match('siapa nama(.*) yang lahir di (.*)',masukan.lower())
+if valid==True:
+    lower = preprocessing.get_input(preprocessing,masukan)
 
-hasil_json = preprocessing.send_querys(preprocessing,query)
+    pekerjaan,lahir = preprocessing.get_query(preprocessing,lower)
 
-value = preprocessing.get_hasil(preprocessing,hasil_json)
+    query = preprocessing.set_sparql(preprocessing,pekerjaan,lahir)
+
+    hasil_json = preprocessing.send_querys(preprocessing,query)
+
+    value = preprocessing.get_hasil(preprocessing,hasil_json)
+else:
+    print("salah format")
