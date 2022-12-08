f17897b62d197b319539386c72209301399c89a1
sunaalkayuniaresta <sunaalkayuni@gmail.com>
2022-12-08 17:26:16 +0700

    major: refactor code to object

--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-import re
 from Controller.Preprocessing import Preprocessing
 from flask import Flask, jsonify, request
 from flask_restful import reqparse, Api, Resource
@@ -15,18 +14,23 @@ class Api(Resource):
     def get(self):
         input = request.args.get("sentence")
 
-        get_input = Preprocessing.get_input(input)
-        get_query= Preprocessing.get_query(get_input)
-        set_sparql = Preprocessing.set_sparql(get_query[0], get_query[1])
-        send_querys = Preprocessing.send_querys(set_sparql)
-        get_hasil = Preprocessing.get_hasil(send_querys)
+        preprocessing = Preprocessing()
+
+        preprocessing.setInput(input)
+
+        getInput = preprocessing.getInput()
+        getQuery = preprocessing.getQuery()
+        setSparQL = preprocessing.setSparQL()
+        sendQuery = preprocessing.sendQuery()
+        getHasil = preprocessing.getHasil()
 
         return {
-            "get_input" : get_input,
-            "get_query": get_query,
-            "set_sparql": set_sparql,
-            "send_querys": send_querys,
-            "get_hasil": get_hasil,
+            "input" : input,
+            "getInput" : getInput,
+            "getQuery" : getQuery,
+            "setSparQL":setSparQL,
+            "sendQuery" : sendQuery,
+            "getHasil" : getHasil,
         }
 
 
